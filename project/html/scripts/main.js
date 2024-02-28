5200b6b0da45d2f5cf034f5f58284bc30acf503b
alym <alymyrysmamatov@gmail.com>
2024-02-28 19:57:41 +0600

    my first commit

--- project/html/scripts/main.js
+++ project/html/scripts/main.js
@@ -1,14 +1,14 @@
-const MyHeading = document.querySelector("h1");
-const imgElement = document.querySelector("img");
+// const MyHeading = document.querySelector("h1");
+// const imgElement = document.querySelector("img");
 
-const img1 = "img/Alatoo.png";
-const img2 = "img/jjk.jpg";
-function changePhoto(event){
-    let current = imgElement.getAttribute("src");
-    if(current == img1){
-        imgElement.setAttribute("src",img2);
-    } else {
-        imgElement.setAttribute("src", img1)
-    }
-}
-imgElement.onclick = changePhoto;
+// const img1 = "img/Alatoo.png";
+// const img2 = "img/jjk.jpg";
+// function changePhoto(event){
+//     let current = imgElement.getAttribute("src");
+//     if(current == img1){
+//         imgElement.setAttribute("src",img2);
+//     } else {
+//         imgElement.setAttribute("src", img1)
+//     }
+// }
+// imgElement.onclick = changePhoto;
